726f37d2b2576d20648b234d95b083342cef8cc5
root <root@localhost.localdomain>
2015-09-10 03:54:41 +0530

    first push

---
+++ index.php
@@ -0,0 +1,140 @@
+<?php
+    // MENUBAR preprocessing 
+    $MENUBAR = str_replace("<div class='cms-menuitem'>", "", $MENUBAR);
+    $MENUBAR = str_replace("</div>", " ", $MENUBAR);
+    $MENUBAR = str_replace("</ul>", "", $MENUBAR);
+    $li_list = array_slice(explode('<li', $MENUBAR),  1,100);
+    $new_menubar = "";
+    foreach ($li_list as $key => $value) {
+        $new = explode("<a", $value)[1];
+        $new = ' <a class="pure-menu-link" ' . $new;
+        $new = '<li class="pure-menu-item">' . $new;
+        $new_menubar .= $new;
+    }
+    $MENUBAR = $new_menubar;  
+?>
+
+<?php
+    // CONTENT preprocessing 
+    //echo htmlentities($CONTENT);
+    //exit();
+
+?>
+
+<?php
+if(!defined('__PRAGYAN_CMS'))
+{ 
+    header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+    echo "<h1>403 Forbidden<h1><h4>You are not authorized to access the page.</h4>";
+    echo '<hr/>'.$_SERVER['SERVER_SIGNATURE'];
+    exit(1);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php $cmstitle=$TITLE;echo $cmstitle; ?></title>
+<meta name="description" content="<?php echo $SITEDESCRIPTION ?>" />
+<meta name="keywords" content="<?php echo $SITEKEYWORDS.', '.$PAGEKEYWORDS ?>" /> 
+<?php global $urlRequestRoot;   global $PAGELASTUPDATED;
+    if($PAGELASTUPDATED!="")
+        echo '<meta http-equiv="Last-Update" content="'.substr($PAGELASTUPDATED,0,10).'" />'."\n";
+?>
+
+<link rel="stylesheet" href="<?php echo $TEMPLATEBROWSERPATH; ?>/pure-min.css">
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="/combo/1.18.13?/css/layouts/side-menu-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $TEMPLATEBROWSERPATH; ?>/main.css">
+    <!--<![endif]-->
+<!--[if lt IE 9]>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+<![endif]-->
+<!--[if lte IE 8]>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $TEMPLATEBROWSERPATH; ?>/grids-responsive-min.css">
+	<!--<![endif]-->
+
+</script>
+<script language="javascript" type="text/javascript" src="<?php echo  $TEMPLATEBROWSERPATH; ?>/jquery-latest.js" ></script>
+<script type="text/javascript" src="<?php echo $TEMPLATEBROWSERPATH; ?>/script.js"></script>
+<script language="javascript" type="text/javascript">
+    //defined here for use in javascript
+    var templateBrowserPath = "<?php echo $TEMPLATEBROWSERPATH ?>";
+    var urlRequestRoot = "<?php echo $urlRequestRoot?>";
+</script>
+</head>
+<body onload="<?php echo $STARTSCRIPTS; ?>">
+
+<div class="header">
+</div>
+<img id="topPillar" src="<?php echo $TEMPLATEBROWSERPATH; ?>/topPillar.png">
+<img id="bottomPillar" src="<?php echo $TEMPLATEBROWSERPATH; ?>/bottomPillar.png">
+<div id="pillarPng"></div>
+<div id="menuShadow"></div>
+<div id="layout">
+    <!-- Menu toggle -->
+    <a href="http://purecss.io/layouts/side-menu/#menu" id="menuLink" class="menu-link">
+        <!-- Hamburger icon -->
+        <span></span>
+    </a>
+
+	<div id="menu">
+        <div class="pure-menu">
+<!--            <a class="pure-menu-heading" href="http://purecss.io/layouts/side-menu/#">Pormaboku</a> -->
+
+            <ul class="pure-menu-list animated slideInLeft">
+                <?php echo $MENUBAR; ?>
+                
+            </ul>
+        </div>
+    </div>
+
+    <div id="main">
+
+        <div class="content cms-content">
+
+            <h2 class="content-subhead"></h2>
+            <?php echo $INFOSTRING; ?>
+            <?php echo $WARNINGSTRING;?>
+            <?php echo $ERRORSTRING; ?>
+	    <div class="pure-g clusters">
+	    	<div class="pure-u-1 pure-u-md-1-5" ></div>
+	    	<div class="pure-u-1 pure-u-md-1-5" ></div>
+	    	<div class="pure-u-1 pure-u-md-1-5" ></div>
+	    	<div class="pure-u-1 pure-u-md-1-5" ></div>
+	    	<div class="pure-u-1 pure-u-md-1-5" ></div>
+	    </div>
+        </div>
+    </div>
+</div>
+    <table id="bottomLinks" cellspacing="10">
+    	<tr>
+            <?php
+                global $userId;
+                if($userId!=0){
+                    echo '<td class="bottomLink"><a href="./+profile">Festember ID:'.$userId.'</a></td>';
+                    echo '<td class="bottomLink"><a href="./+logout">Logout</a></td>';
+                }
+                else{
+                    echo '<td class="bottomLink"><a href="./+login">Login</a></td>';
+                    echo '<td class="bottomLink"><a href="./+login&subaction=register">Register</a></td>';
+                }
+            ?>
+    	</tr>
+    </table>
+
+
+
+
+<script src="<?php echo $TEMPLATEBROWSERPATH; ?>/jquery-2.1.4.min.js"></script>
+<script src="<?php echo $TEMPLATEBROWSERPATH; ?>/main.js"></script>
+
+
+
+</body></html>
---
+++ main.js
@@ -0,0 +1,88 @@
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    menuLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+
+    };
+	layoutSpecs();
+	
+	var arr1 = $("a.pure-menu-link");
+	var arr2 = $("div.tabElement > a");
+	$(arr1[0]).addClass("menuLinkActive");
+	if(arr2.length >0){
+		$("div.tabElement").css("display","none");
+		for(var i=0;i<arr1.length;i++){
+			arr1[i].onclick = function(e){
+				e.preventDefault();
+				var j=0;
+				for(j=0;j<arr1.length;j++){
+					console.log(this,arr1[j]);
+					if(this == arr1[j])
+						break;
+				}
+				$(arr1).removeClass("menuLinkActive");
+				$(arr1[j]).addClass("menuLinkActive");
+				$(arr2[j]).trigger("click");
+			}
+		}
+	}
+}(this, this.document));
+
+window.onresize = function(){
+	layoutSpecs();
+}
+function layoutSpecs(){
+
+    var layoutHeight = $(window).innerHeight() - $(".header").outerHeight();
+    layoutHeight = layoutHeight > 500?500:layoutHeight;
+    $("#layout").outerHeight(layoutHeight);
+    $(".content").outerHeight(layoutHeight - 50);
+    var pos = $(".content").offset();
+    if(pos.left - 150 >= 120){
+	   $("#pillarPng").css("display","block");
+	   $("#pillarPng").css("left",150+(pos.left-150-100)/2);
+
+	   $("#topPillar").css("display","block");
+	   $("#topPillar").css("left",150+(pos.left-150-100)/2-17);
+
+	   $("#bottomPillar").css("display","block");
+	   $("#bottomPillar").css("left",150+(pos.left-150-100)/2-19);
+
+	   $("#menuShadow").css("display","block");
+	   $("#menuShadow").css("width",150+(pos.left-150-100)/2+25);
+   }else{
+	   $("#pillarPng").css("display","none");
+	   $("#topPillar").css("display","none");
+	   $("#bottomPillar").css("display","none");
+	   $("#menuShadow").css("display","none");
+   }
+   
+}
